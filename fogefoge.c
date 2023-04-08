82d53a10bd5d1a90fe945493fc6a3ec24a545f18
Erikrmb <erikrmb@gmail.com>
2023-04-08 15:12:23 -0400

    Deploy FogeFoge using C

---
+++ fogefoge.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+char** mapa;
+int linhas;
+int colunas;
+
+
+void liberaMapa(){
+    for(int i = 0; i < linhas; i++){
+        free(mapa[i]);
+    }
+    free(mapa);
+}
+
+
+void alocaMapa(){
+    mapa = malloc(sizeof(char*) * linhas);
+    for(int i = 0; i < linhas; i++){
+        mapa[i] = malloc(sizeof(char) * (colunas + 1));
+    }
+}
+
+void leMapa(){
+    
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+    if(f == 0){
+        printf("Erro na leitura do mapa\n");
+        exit(1);
+    }
+
+    fscanf(f, "%d %d", &linhas, &colunas);
+
+    alocaMapa();
+
+    for(int i = 0; i < 5; i++){
+        fscanf(f, "%s", mapa[i]);
+    }
+    
+    fclose(f);
+
+}
+
+void imprimeMapa(){
+    for(int i = 0; i < linhas; i++){
+        printf("%s\n", mapa[i]); 
+    }
+}
+
+int acabou(){
+    return 0;
+}
+
+void move(char direcao){
+    int x;
+    int y;
+    
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; i < colunas; j++){
+            if(mapa[i][j] == '@'){
+                x = i;
+                y = j;
+                break;
+            }
+        }
+    }
+
+    switch (direcao){
+        case 'a':
+            mapa[x][y-1] = '@';
+            break;
+        case 'w':
+            mapa[x-1][y] = '@';
+            break;
+        case 's':
+            mapa[x+1][y] = '@';
+            break;
+        case 'd':
+            mapa[x][y+1] = '@';
+            break;
+    }
+
+    
+    mapa[x][y] = '.';
+
+}
+
+int main() {
+
+    leMapa();
+
+
+    do
+    {
+        imprimeMapa();
+        
+        char comando;
+        scanf(" %c", &comando);
+        move(comando);
+
+    } while (!acabou());
+    
+
+    liberaMapa();
+}
---
+++ fogefoge.h
@@ -0,0 +1,7 @@
+void alocaMapa();
+void leMapa();
+void liberaMapa();
+
+void move(char direcao);
+int acabou();
+void imprimeMapa();
